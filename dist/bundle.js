4fe100a00b7c84fc4bc97806e53a8f2016a66cfa
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2021-08-26 11:34:28 -0300

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+"use Strict";
+
+var _user$address;
+
+var user = {
+    name: 'Paulo' ,
+}
+
+console.log((_user$address = user.address) == nul || _user$address == void 0  ? void 0 : _user$address.street)
