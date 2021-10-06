fdb1bac63ebbfd0a4afbf5b9d60442af1ee3eb0e
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2021-10-06 20:33:27 -0300

    Add files via upload

--- Counter.jsx
+++ Counter.jsx
@@ -1,12 +1,12 @@
 export function Counter() {
-    function Increment() {
+    function increment() {
         console.log('incrementing')
     }
 
     return (
         <div>
             <h2>0</h2>
-            <button type="button">Increment</button>
+            <button type="button" onClick={increment}>Increment</button>
         </div>
     );
 }
