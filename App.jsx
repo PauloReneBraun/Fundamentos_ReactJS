94904722508aaa18b12132c3d4a45298aeec4a6c
Paulo Braun - Frontend Developer - React <66573504+PauloReneBraun@users.noreply.github.com>
2021-10-06 20:20:50 -0300

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,9 +1,13 @@
+import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
     throw new Error(' Puts grila Nilce')
     return (
+        <>
         <RepositoryList />
+        <Counter />
+        </>
     )
 }
